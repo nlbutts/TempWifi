dedaf350d97a8165cdea14fc90d7fb89176579b5
Nicholas Butts <nlbutts@ieee.org>
2016-03-05 20:16:47 -0600

    Got a C++ version of the SendMail program working.

---
+++ SendEmail/NTPTime.h
@@ -0,0 +1,13 @@
+#include <time.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+uint8_t GetTime(struct tm * decodedTime);
+
+#ifdef  __cplusplus
+}
+#endif /* __cplusplus */
+
+
